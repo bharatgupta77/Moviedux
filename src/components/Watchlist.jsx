74ee0b0d57b9defb9e70a1be27f12c18417e24e0
Bharat Gupta <guptabharat297@gmail.com>
2026-03-17 22:31:33 -0500

    added Watchlist.jsx

---
+++ src/components/Watchlist.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Watchlist(movies, watchlist,  toggleWatchList) {
+    return (
+        <section>
+            <h2>Your Watchlist</h2>
+        </section>
+    );
+}
+
+export default Watchlist;
